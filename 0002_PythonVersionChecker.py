af82303ba0d7c5229a3818eed9d6f0aaca544d9e
Peter Rake <prake71@gmail.com>
2025-06-27 20:28:53 +0200

    added exercise 2 and 3

---
+++ 0002_PythonVersionChecker.py
@@ -0,0 +1,19 @@
+'''
+from: https://www.w3resource.com/python-exercises/python-basic-exercises.php
+
+2. Python Version Checker
+
+Write a Python program to find out what version of Python you are using.
+
+'''
+import sys
+
+# Knowing your Python version is crucial to ensure that your code runs
+# as expected. If you’re using a feature that’s only available in a
+# newer version, your code won’t run on an older version. Here’s how
+# you can check your Python version:
+
+
+print(sys.version)
+
+ 
