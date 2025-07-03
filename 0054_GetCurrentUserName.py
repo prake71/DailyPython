b7af217052268b90b2ca9d5625bfce262c703376
Peter Rake <peter.rake@bbraun.com>
2025-07-03 18:17:40 +0200

    added exercise 54

---
+++ 0054_GetCurrentUserName.py
@@ -0,0 +1,14 @@
+'''
+from: https://www.w3resource.com/python-exercises/python-basic-exercises.php
+
+54. Get Current Username
+
+Write a Python program to get the current username.
+Click me to see the sample solution
+'''
+
+import os
+
+print(os.getlogin())
+
+
