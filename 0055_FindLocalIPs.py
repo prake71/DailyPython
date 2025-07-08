516558c0685f84a77a98bab5cd656f45f86fde21
Peter Rake <peter.rake@bbraun.com>
2025-07-08 18:20:53 +0200

    exercise 55

---
+++ 0055_FindLocalIPs.py
@@ -0,0 +1,21 @@
+'''
+from: https://www.w3resource.com/python-exercises/python-basic-exercises.php
+
+55. Find Local IPs
+
+Write a Python program to find local IP addresses using Python's stdlib.
+
+
+'''
+import os # for alternative 1
+import socket # for alternative 2
+
+
+# alternative 1
+print(os.system('ipconfig'))
+
+# alternative 2
+# https://docs.python.org/3/library/socket.html
+
+
+
