76e82e6d7ec7e74f666a6e38ced87c5dfdc5cc6a
Peter Rake <prake71@gmail.com>
2025-07-30 22:45:19 +0200

    started with  exercise 62, to be continued

---
+++ 0062_TimeToSecondsConverter.py
@@ -0,0 +1,23 @@
+'''
+from: https://www.w3resource.com/python-exercises/python-basic-exercises.php
+
+62. Time to Seconds Converter
+
+Write a Python program to convert all units of time into seconds. 
+'''
+
+# units of time: millenium, century, decade, year, day, hour, minutes, seconds
+# see also: https://en.wikipedia.org/wiki/Unit_of_time
+
+# 1 minute = 60 secs
+# 1 hour = 60 mins x 60 secs
+# 1 day = 24 hrs x 60 mins x 60 secs
+# 1 week = 7 days x 24 hrs x 60 mins x 60 secs
+# 1 month = 4 weeks x 7 days x 24 hrs x 60 mins x 60 secs
+# 1 year = 12 months x 4 weeks x 7 days x 24 hrs x 60 mins x 60 secs
+# 1 decade = 10 years x 12 months x 4 weeks x 7 days x 24 hrs x 60 mins x 60 secs
+# 1 century = 10 decades x 10 years x 12 months x 4 weeks x 7 days x
+#             24 hrs x 60 mins x 60 secs
+# 1 millenium = 10 centuries x 10 decades x 12 months x 4 weeks x 7
+#               days x 24 hrs x 60 mins x 60 secs
+
